695b296a11056b7ce8a5747ecd804774c0cc1e4c
mashi <mashi.zzz@gmail.com>
2023-01-24 08:04:28 +0900

    データめくりの処理が間違っていたので修正した。あと、コメントを整理した。

--- get_blood_data.py
+++ get_blood_data.py
@@ -1,3 +1,29 @@
+'''
+ラブラッドのサイトから、2009.3.15以降の献血データを取得するプログラムです。
+実行には、本ファイルと同じ場所にログイン用のパスワードなどが記載された「.password」ファイルが必要です。
+「.password」ファイルには、
+
+BLOODCODE = 'xxxxxxxxxx'
+PASSWORD = 'xxxxxxxx'
+RECORDPASSWORD = 'xxxx'
+
+と記述します。
+
+実行すると、Chromeが立ち上がりログイン処理、血液の分析結果の取得などが行われ、スクリプトと同じフォルダに「blood_data.csv」が作成されます。
+
+2009.3.15以降のデータを取得することとしたのは、血液の分析結果の項目が2009.3.15以降変更となっているためで
+処理が面倒なので、このプログラムでは2009.3.15以降のデータのみ取得することとしました。
+
+*** エラー関連 ***
+実行すると、
+selenium.common.exceptions.StaleElementReferenceException: Message: stale element reference: element is not attached to the page document
+
+とエラー表示された際は、待ち時間をより長く設定してみてください。
+本来は、状態変更をみて設定するのが良いのでしょうが、現状ではそこまでは行っていません。
+
+そのうち、取得時間の節約やスキルアップのために、明示的な待機（WebDriverWait.until()）などを組み込むかもしれません。
+
+'''
 import time
 import collections
 import numpy as np
@@ -5,7 +31,7 @@ import pandas as pd
 from tabulate import tabulate
 from datetime import datetime as dt
 from datetime import date
-import math
+
 # dotenvに必要
 import os
 from dotenv import load_dotenv
@@ -25,9 +51,9 @@ driver = webdriver.Chrome()
 
 # 待ち時間の設定
 # ページ遷移を伴う場合
-wait_for_page = 1.7
+wait_for_page = 2
 # ページ遷移を伴わない場合
-wait_for_nopage = 0.7
+wait_for_nopage = 1
 
 
 def login_and_get_param():
@@ -45,12 +71,6 @@ def login_and_get_param():
     """
 
     # ログイン用のパスワードなどを別ファイル(.password)から読み込み
-    ''' .passwordファイルには
-    BLOODCODE = 'xxxxxxxxxx'
-    PASSWORD = 'xxxxxxxx'
-    RECORDPASSWORD = 'xxxx'
-    と記述する。
-    '''
     load_dotenv('.password')
     BLOODCODE = os.environ.get("BLOODCODE")
     PASSWORD = os.environ.get("PASSWORD")
@@ -68,13 +88,10 @@ def login_and_get_param():
     time.sleep(wait_for_nopage)
     driver.find_element(By.LINK_TEXT, '献血記録を確認する').click()
     time.sleep(wait_for_nopage)
-    #
+    
     # ログイン後、献血記録確認のパスワードを入力すると
     # 「献血記録を確認」の初期画面として、最新のデータから3回分の血液などのデータが表示される。
     # このページには過去全ての献血日と種別が含まれ、表示してる3回分のデータのみ aria-hidden="false" となっている。
-    # また、血液の分析結果の項目が2009.3.15以降変更となっている。
-    # 
-    # このツールでは2009.3.15以降のデータのみ取得することとする。
     # 2009.3.15以降の献血回数の取得
     date_of_kenketsu = driver.find_elements(By.CLASS_NAME, 'mod-past-data__date') # 献血回数（すべての献血日の数を取得）
     num_of_kenketsu_all = len(date_of_kenketsu)
@@ -100,7 +117,7 @@ def get_data(times, num_of_kenketsu_all):
     
     Args:
         times (int): データを取得する回数
-        num_of_kenketsu_all (int): 過去に献血した回数、最新のデータがリストの最も後ろになっているため、forループの開始用に使用する。
+        num_of_kenketsu_all (int): 過去に献血した回数、最新のデータがリストの最も後ろになっているため、日付を取得するforループの開始用に使用する。
 
     Returns:
         kenketsu_data_reshape : 表示されている3回分データを整形してnumpyの配列として返す
@@ -118,7 +135,7 @@ def get_data(times, num_of_kenketsu_all):
     for i in range(num_of_kenketsu_all+2-(times*3), num_of_kenketsu_all+2-((times+1)*3), -1):
         index.append(dt.strptime(date_of_kenketsu[i].text, '%Y/%m/%d')) # 日付の文字列をdatetimeに変換
         kenketsu_data.append(kenketsu_kind[i].text)
-    # 19種類のデータ分ループして値を取り込む
+    # 18種類のデータ分ループして値を取り込む
     kenketsu_raw_data = driver.find_elements(By.CLASS_NAME, 'mod-result-table__data')
     for i in range(len(kenketsu_raw_data)):
         kenketsu_data.append(kenketsu_raw_data[i].text.split()) # [a b c]となっているので、splitが必要
@@ -129,11 +146,25 @@ def get_data(times, num_of_kenketsu_all):
 
 
 def main():
+    # ログインして2009.3.15以降とすべての献血回数を求める。
     num_of_kenketsu, num_of_kenketsu_all = login_and_get_param()
-    # 血液データの種別を格納するリストをもとにdataframeを作成
+    # 血液データの種別を格納するリストをもとにdataframeを作成する。
     cols = ['献血種別','血圧（最高）','血圧（最低）','脈拍','ALT（GPT）','γ-GTP','総蛋白TP','アルブミンALB','ALB/G','CHOL','GALB','RBC','Hb','Ht','MCV','MCH','MCHC','WBC','PLT']
-    df = pd.DataFrame(columns=cols)
-    turn_num = math.ceil(num_of_kenketsu/3)
+    df = pd.DataFrame(columns=cols) # データを作成する。
+    # 2009.3.15以降の最も古い献血データを表示するまでデータをすすめる。
+    # データは3つずつ進めているので、以下の処理とする。
+    # この処理により、
+    # 81回だと27回のめくりで、2009.3.15より前のデータは含まれない。
+    # 2009.3.15より前のデータは、それぞれ、82回だと2回分、83回だと1回分、84回だと0回分含まれることとなる。
+    if num_of_kenketsu % 3 == 0:
+        turn_num = num_of_kenketsu // 3
+    elif num_of_kenketsu % 3 == 1:
+        turn_num = (num_of_kenketsu + 2) // 3
+    elif num_of_kenketsu % 3 == 2:
+        turn_num = (num_of_kenketsu + 1) // 3
+    else:
+        pass
+    # 1からturn_numまでループ処理にて、データの取得 → データのめくり（重複しないよう3回分）を行なう。
     for times in range(1,turn_num+1,1):
         kenketsu_data_reshape, index = get_data(times, num_of_kenketsu_all)
         # dfへデータを格納
@@ -141,7 +172,7 @@ def main():
         # チェック用にdfを表示する。
         # print(tabulate(df, df.columns,tablefmt='github', showindex=True))
         time.sleep(wait_for_page)
-        # データを取得してページを進めており、最後のデータを取得した後にページを進める必要はないためにif文を入れている。
+        # データを取得してページを進めており、最後のデータを取得した後にページを進める必要はないため以下の処理を入れている。
         if times < turn_num:
             # 献血データが重複しないよう3つずつすすめる。
             for i in range(3):
@@ -152,12 +183,17 @@ def main():
             time.sleep(wait_for_nopage) # ページの遷移を待って、次の処理へ
         else:
             pass
+    '''
+    *** 2009.3.15以前のデータの紛れ込みについて ***
+    2009.3.15以降の献血回数が3で割り切れないとき、古いデータが最大2行紛れ込む可能性がある。
+    したがって、ココで改めて2009.3.15以降のデータを抽出している。
+    
+    ラブラッドのサイトでは、2009.3.15以降と以前のデータを同時に表示する場合、データ数は19種類として表示され該当データがない箇所は'-'となる。データの取得は、最新のデータ表示にて取得したデータ種（18）のループとして回しているため途中でデータ数が19種になってもエラーなくデータが取得されるが、2009.3.15より前のデータは項目とその順番が異なるため、一部のデータがcolsで指定した列と異なるデータとなる。
+    '''
     date = dt(2009, 3, 14)
-    # 2009/3/15以降の献血回数が3で割り切れないとき、古いデータが最大2行紛れ込む可能性があるため以下の処理を追加する。
     df = df[df.index > date]
     print(tabulate(df, df.columns,tablefmt='github', showindex=True))
     df.to_csv('./blood_data.csv')
-    time.sleep(wait_for_page)
     driver.quit()
 
 if __name__ == "__main__":
