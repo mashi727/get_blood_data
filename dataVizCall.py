5bed17825bdb0bdd1c785e618e0028cac71b9309
mashi <mashi.zzz@gmail.com>
2023-01-27 15:45:42 +0900

    データ可視化ツール、標準値を表示する機能などを追加

--- dataVizCall.py
+++ dataVizCall.py
@@ -13,85 +13,291 @@ from datetime import datetime
 from dataVizUi import Ui_MainWindow
 
 class TimeAxisItem(pg.AxisItem):
+    '''
+    時間をを表示する。
+    UNIX時間を以下の様にJSTに変換する。
+    pd.to_datetime(df['date']).dt.tz_localize(tz='Asia/Tokyo')
+    self.dt = df.index.astype(np.int64)//10**9
+    PlotWidgetの中で、以下の用に使用する
+    pg.PlotWidget(axisItems={'bottom': TimeAxisItem(orientation='bottom')})
+    '''
     def __init__(self, *args, **kwargs):
         super(TimeAxisItem, self).__init__(*args, **kwargs)
-
+    
     def tickStrings(self, values, scale, spacing):
-        # %Yは、yyyyを表すみたい
+        # return [datetime.fromtimestamp(v).strftime('%Y %m/%d %H:%M') for v in values]
         return [datetime.fromtimestamp(v).strftime('%Y %m/%d') for v in values]
+        '''
+        フォーマットの例
+        %Y：西暦 ( 4桁) の 10 進表記を表します。
+        %m：0埋めした10進数で表記した月。
+        %d：0埋めした10進数で表記した月中の日にち。
+        %H：0埋めした10進数で表記した時 (24時間表記)。
+        %I：0埋めした10進数で表記した時 (12時間表記)。
+        %M：0埋めした10進数で表記した分。
+        %S：0埋めした10進数で表記した秒。
+        %b：ロケールの月名を短縮形で表示します。
+        %B：ロケールの月名を表示します。
+        
+        詳細はこちら。
+        https://docs.python.org/ja/3/library/datetime.html#strftime-and-strptime-behavior
+        '''
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.show()
         self.init_ui()
-        self.win = self.graphicsView
-        
+
         df = pd.read_csv('./blood_data_sorted.csv', index_col=0,usecols=[0,*range(2, 20)])
         self.df = df.set_index(pd.to_datetime(df.index, format='%Y-%m-%d'))
         self.dt = self.df.index.astype(np.int64)//10**9
-        #print(self.dt)
+
+        self.cols=df.columns
+        self.num_of_cols = 5
+        minx = self.dt[-30]
+        maxx = self.dt[-1]
+
+        self.set_graph_ui(self.cols, self.num_of_cols,minx,maxx)
+        self.plot_region(self.num_of_cols)
+        self.region.sigRegionChanged.connect(self.update_region_change)
+        #self.region.sigRegionChanged.connect(lambda: self.update_region_change())
+        self.region.setRegion([minx, maxx])
+
         
-        cols=df.columns
+
+    def init_ui(self):
+        # Windowサイズを設定
+        self.setGeometry(100, 100, 2280, 1768)
+
+
+    def plot_region(self, num_of_cols):
+        setprop_region = lambda x: (x.setAutoVisible(y=True),
+                                    x.enableAutoRange('y'),
+                                    x.showAxis('right'),
+                                    x.hideAxis('left'), 
+                                    x.getAxis('right').setHeight(0))
+        region = pg.LinearRegionItem() # http://www.pyqtgraph.org/documentation/graphicsItems/linearregionitem.html
         
-        #cols = ['血圧（最高）','血圧（最低）','脈拍','ALT（GPT）','γ-GTP','総蛋白TP','アルブミンALB','ALB/G','CHOL','GALB','RBC','Hb','Ht','MCV','MCH','MCHC','WBC','PLT']
+        plt_region = self.graphicsView.addPlot(axisItems={'bottom': TimeAxisItem(orientation='bottom')},row=4,col=0, colspan=num_of_cols)
+        plt_region.layout.setRowStretchFactor(4, 1)
+        plt_region.addItem(region, ignoreBounds=True)
+        setprop_region(plt_region)
+        plt_region.plot(x=self.dt, y=self.df['血圧（最高）'])
+
+        self.region = region
         
+    def set_graph_ui(self,cols, num_of_cols, minx, maxx):
+        # colsは、表示するデータの種類（以下のとおり）
+        # '血圧（最高）', '血圧（最低）', '脈拍', 'ALT（GPT）', 'γ-GTP', '総蛋白TP', 'アルブミンALB', 'ALB/G', 'CHOL', 'GALB', 'RBC', 'Hb', 'Ht', 'MCV', 'MCH', 'MCHC', 'WBC', 'PLT'
+        # 18種あります。
+        #
+        # num_of_colsは、表示するグラフのカラム数
+        # minx,maxxは、初期表示のX軸の範囲
         graph_obj = []
         for i in range(len(cols)):
             graph_obj.append('p'+str(f"{i}"))
 
-        num_of_fold = 5
+        # 折返し用に行数を算出
+        if len(graph_obj) % num_of_cols == 0:
+            num_of_rows = len(graph_obj)//num_of_cols
+        else:
+            num_of_rows = (len(graph_obj)//num_of_cols) + 1
+
+        # レイアウト関連
+        setprop = lambda x: (x.showGrid(x=True, y=True, alpha = 1),
+                             x.enableAutoRange('y'),
+                             x.addLegend(),
+                             x.hideAxis('left'), 
+                             x.getAxis('right').setHeight(0))
+
+
+        self.graph_id_num = []
+        for i in range(len(cols)):
+            graph_obj[i] = self.graphicsView.addPlot(axisItems={'bottom': TimeAxisItem(orientation='bottom')})
+            self.graph_id_num.append(id(graph_obj[i]))
+            #graph_id.setXRange(*self.region.getRegion(), padding=0)
+            graph_obj[i].setXRange(minx,maxx, padding=0)
+            graph_label = cols[i]
+            setprop(graph_obj[i])
+            #graph_obj[i].setTitle('<font size=\'5\' color=\'#FFFFFF\'>'+ '献血結果' +'</font>')
+            styles = {'color':'white','font-size':'20px', 'font-style':'bold'}
+            graph_obj[i].setLabel('left', text=graph_label, units='', **styles)
+
+            self.plot_xy(graph_label, graph_obj[i])
+            graph_obj[i].layout.setRowStretchFactor(i, 15)
+            if (i + 1) % num_of_cols == 0:
+                self.graphicsView.nextRow()
+
+    # データを扱うコードのみを記述
+    def plot_xy(self, cols, graph_id):
+        s = self.df[cols]
+        s_rolling = self.df[cols].rolling(5).mean()
+        graph_id.addItem(pg.PlotDataItem(self.dt, s, pen='y', symbol='o', symbolBrush=(255, 255, 0,100)))
+        graph_id.addItem(pg.PlotDataItem(self.dt, s_rolling, pen=pg.mkPen((255,255,255,128), width=5)))
+        if cols == 'GALB':
+            graph_id.setYRange(10,17)
+            inf_upper = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}%')
+            inf_upper.setPos(pg.Point(0, 16.5))
+            graph_id.addItem(inf_upper)
+        elif cols == 'ALT（GPT）':
+            inf_upper = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}IU/L')
+            inf_upper.setPos(pg.Point(0, 8))
+            graph_id.addItem(inf_upper)
+            inf_lower = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}IU/L')
+            inf_lower.setPos(pg.Point(0, 49))
+            graph_id.addItem(inf_lower)
+        elif cols == 'γ-GTP':
+            inf_upper = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}IU/L')
+            inf_upper.setPos(pg.Point(0, 9))
+            graph_id.addItem(inf_upper)
+            inf_lower = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}IU/L')
+            inf_lower.setPos(pg.Point(0, 68))
+            graph_id.addItem(inf_lower)
+        elif cols == '総蛋白TP':
+            inf_upper = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}g/dL')
+            inf_upper.setPos(pg.Point(0, 6.6))
+            graph_id.addItem(inf_upper)
+            inf_lower = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}g/dL')
+            inf_lower.setPos(pg.Point(0, 8.2))
+            graph_id.addItem(inf_lower)
+        elif cols == 'アルブミンALB':
+            inf_upper = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}g/dL')
+            inf_upper.setPos(pg.Point(0, 4.0))
+            graph_id.addItem(inf_upper)
+            inf_lower = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}g/dL')
+            inf_lower.setPos(pg.Point(0, 5.1))
+            graph_id.addItem(inf_lower)
+        elif cols == 'ALB/G':
+            inf_upper = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}')
+            inf_upper.setPos(pg.Point(0, 1.3))
+            graph_id.addItem(inf_upper)
+            inf_lower = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}')
+            inf_lower.setPos(pg.Point(0, 2.1))
+            graph_id.addItem(inf_lower)
+        elif cols == 'CHOL':
+            inf_upper = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}mg/dL')
+            inf_upper.setPos(pg.Point(0, 140))
+            graph_id.addItem(inf_upper)
+            inf_lower = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}mg/dL')
+            inf_lower.setPos(pg.Point(0, 259))
+            graph_id.addItem(inf_lower)
+        elif cols == 'RBC':
+            inf_upper = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}x10^4/μL')
+            inf_upper.setPos(pg.Point(0, 560))
+            graph_id.addItem(inf_upper)
+            inf_lower = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}x10^4/μL')
+            inf_lower.setPos(pg.Point(0, 418))
+            graph_id.addItem(inf_lower)
+        elif cols == 'Hb':
+            inf_upper = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}g/dL')
+            inf_upper.setPos(pg.Point(0, 17.0))
+            graph_id.addItem(inf_upper)
+            inf_lower = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}g/dL')
+            inf_lower.setPos(pg.Point(0, 12.7))
+            graph_id.addItem(inf_lower)
+        elif cols == 'Ht':
+            inf_upper = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}%')
+            inf_upper.setPos(pg.Point(0, 50.0))
+            graph_id.addItem(inf_upper)
+            inf_lower = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}%')
+            inf_lower.setPos(pg.Point(0, 38.8))
+            graph_id.addItem(inf_lower)
+        elif cols == 'MCV':
+            inf_upper = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}fL')
+            inf_upper.setPos(pg.Point(0, 99.5))
+            graph_id.addItem(inf_upper)
+            inf_lower = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}fL')
+            inf_lower.setPos(pg.Point(0, 83.0))
+            graph_id.addItem(inf_lower)
+        elif cols == 'MCH':
+            inf_upper = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}pg')
+            inf_upper.setPos(pg.Point(0, 33.5))
+            graph_id.addItem(inf_upper)
+            inf_lower = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}pg')
+            inf_lower.setPos(pg.Point(0, 26.8))
+            graph_id.addItem(inf_lower)
+        elif cols == 'MCHC':
+            inf_upper = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}%')
+            inf_upper.setPos(pg.Point(0, 35.2))
+            graph_id.addItem(inf_upper)
+            inf_lower = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}%')
+            inf_lower.setPos(pg.Point(0, 31.7))
+            graph_id.addItem(inf_lower)
+        elif cols == 'MCHC':
+            inf_upper = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}%')
+            inf_upper.setPos(pg.Point(0, 35.2))
+            graph_id.addItem(inf_upper)
+            inf_lower = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}%')
+            inf_lower.setPos(pg.Point(0, 31.7))
+            graph_id.addItem(inf_lower)
+        elif cols == 'WBC':
+            inf_upper = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}x10^2/μL')
+            inf_upper.setPos(pg.Point(0, 89))
+            graph_id.addItem(inf_upper)
+            inf_lower = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}x10^2/μL')
+            inf_lower.setPos(pg.Point(0, 38))
+            graph_id.addItem(inf_lower)
+        elif cols == 'PLT':
+            inf_upper = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}x10^4/μL')
+            inf_upper.setPos(pg.Point(0, 36.5))
+            graph_id.addItem(inf_upper)
+            inf_lower = pg.InfiniteLine(movable=False, angle=0, pen=(255, 0, 0), hoverPen=(0,200,0),label='{value:0.2f}x10^4/μL')
+            inf_lower.setPos(pg.Point(0, 17.0))
+            graph_id.addItem(inf_lower)
+        else:
+            pass
+
+        # 基準値 
+        # 血圧（最高）
+        # 血圧（最低）
+        # 脈拍
+        # 3 ALT（GPT）基準値: 8～49 IU/L
+        # 4 γ-GTP 基準値: 9～68 IU/L
+        # 5 '総蛋白TP', 総蛋白 TP 基準値: 6.6～8.2 g/dL
+        # 6 'アルブミンALB', アルブミン ALB 基準値: 4.0～5.1 g/dL
+        # 7 'ALB/G', アルブミン対グロブリン比 A/G 基準値: 1.3～2.1
+        # 8 'CHOL', コレステロール CHOL 基準値: 140～259 mg/dL
+        # 10 'RBC', 赤血球数 RBC 基準値(男性): 418～560 x10^4 /μL
+        # 11 'Hb', ヘモグロビン量 Hb 基準値(男性): 12.7～17.0 g/dL
+        # 12 'Ht', ヘマトクリット値 Ht 基準値(男性): 38.8～50.0 %
+        # 13 'MCV', 平均赤血球容積 MCV 基準値: 83.0～99.5 fL
+        # 14 'MCH', 平均赤血球ヘモグロビン量 MCH 基準値: 26.8～33.5 pg
+        # 15 'MCHC', 平均赤血球ヘモグロビン濃度 MCHC 基準値: 31.7～35.2 %
+        # 16 'WBC', 白血球数 WBC 基準値: 38～89 x10^2 /μL
+        # 17 'PLT', 血小板数 PLT 基準値: 17.0～36.5 x10^4/μL 
+        # 
+        # このうち、cols[]が '血圧（最高）', '血圧（最低）', '脈拍'は、基準なし
+        #
+        # 'GALB'は、上限値のみ
+        ## 9 グリコアルブミンGA 基準値: 16.5 % 未満
+        #
+        # ほかは、上下限値を記載
+        # 'ALT（GPT）', 'γ-GTP', '総蛋白TP', 'アルブミンALB', 'ALB/G', 'CHOL', 'RBC', 'Hb', 'Ht', 'MCV', 'MCH', 'MCHC', 'WBC', 'PLT'
+        # # 
         
-        num = len(graph_obj)+1
-        for case in range(1,num):
-            graph_id = graph_obj[case-1]
-            graph_label = cols[case-1]
-            # 凡例のフォントの大きさと、表示するテキストの指定
-            #legend = '<font size=\'5\' color=\'#FFFFFF\'>'+ 'λ='+str(average_num) +'</font>'
-            self.graph_id = self.set_graph_ui(graph_id, case, num_of_fold, graph_label)
-            self.plot_xy(graph_id, graph_label)
-            self.vb = self.graph_id.vb
     
-    def init_ui(self):
-        # Windowサイズを設定
-        self.setGeometry(100, 100, 2300, 1400)
-
-    def set_graph_ui(self,graph_id, num, num_of_fold, graph_label):
-        # いつもは、こちら。
-        setprop = lambda x: (x.showGrid(x=True, y=True, alpha = 1),x.setAutoVisible(y=True), x.addLegend(), x.showGrid(x=True, y=True))
-        # setAutoVisible(y=True)を有効にすると、IndexError: boolean index did not match indexed array along dimension 0;
-        # dimension is 39 but corresponding boolean dimension is 40 がでます。
-        #setprop = lambda x: (x.showGrid(x=True, y=True, alpha = 1), x.addLegend(offset=(0,5.5)), x.showGrid(x=True, y=True))
-        styles = {'color':'white','font-size':'20px', 'font-style':'bold'}
-          
-        graph_id = self.win.addPlot()
-        #graph_id.setTitle('<font size=\'5\' color=\'#FFFFFF\'>'+ 'Histogram' +'</font>')
-        graph_id.setLabel('left', text=graph_label, units='', **styles)
-        #graph_id.setLabel('bottom', text='献血日', units='', **styles)
-        setprop(graph_id)
-        self.graph_id = graph_id
-        if num % num_of_fold == 0:
-            self.win.nextRow()
-        return graph_id
-
-
-
-    def plot_xy(self,graph_id, graph_label):
-        pass
-        s = self.df[graph_label]
-        s_rolling = self.df[graph_label].rolling(3).mean()
-        legend = '<font size=\'5\' color=\'#FFFFFF\'>'+ str(graph_id) +'</font>'
-        # Create a plot with a date-time axis
-        #self.graph_id(pg.PlotWidget(axisItems={'bottom': TimeAxisItem(orientation='bottom')}))
-        #self.graph_id.plot(self.dt, s, name=legend,alpha=1 ,symbolPen=pg.mkPen(color=(255, 255, 0)),symbolBrush=(255, 255, 0,100), symbolSize=10, pen=None, symbol='o')
-        self.graph_id.addItem(pg.PlotDataItem(self.dt, s, pen='w',axisItems={'bottom': TimeAxisItem(orientation='bottom')}))
-        self.graph_id.addItem(pg.PlotDataItem(self.dt, s_rolling, pen=pg.mkPen((255,255,255,128), width=5)))
+    def update_region_change(self, region):
+        #self.graphicsView.clear()
+        import _ctypes
+        for case in range(len(self.graph_id_num)):
+            _ctypes.PyObj_FromPtr(self.graph_id_num[case]).setXRange(*self.region.getRegion(), padding=0)
+        region.setZValue(10)
         
-        pass
+        #print(minx,maxx)
+        #self.plot_xy(self.cols, self.num_of_cols,minx, maxx)
 
 
-app = QApplication(sys.argv)
-window = MainWindow()
-window.show()
-app.exec()
+    def update_region(self, window, viewRange):
+        self.region.setRegion(viewRange[0])
+
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
+
+if __name__ == '__main__':
+    main()
